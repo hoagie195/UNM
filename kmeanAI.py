b023f92b1165393df885f9c6a78a6a2cbf12e7a9
lmhoag <54159994+lmhoag@users.noreply.github.com>
2022-01-12 20:17:57 -0500

    changed dataset to numbers

--- kmeanAI.py
+++ kmeanAI.py
@@ -4,19 +4,11 @@ from matplotlib import colors as mcolors
 import math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import pandas as pd
 class KMEAN:
     def __init__(self):
-        self.data = [
-       [50, 50, 50, 40],
-       [50, 50, 30, 60],
-       [50, 20, 50, 60],
-       [50, 50, 10, 50],
-       [40, 30, 30, 30],
-       [40, 20, 30, 40],
-       [40, 30, 20, 20],
-       [40, 25, 30, 40],
-    ]
-        self.clusters = 3
+        self.data = pd.read_csv("kddcup99_csvnew.csv", encoding='utf-8')
+        self.clusters = 7
         super().__init__()
     
     def predict(self):
@@ -25,44 +17,11 @@ class KMEAN:
         y_km = km.fit_predict(self.data)
         print(km.labels_)
         print(km.inertia_)
-        pca = PCA(2)
+        pca = PCA(3)
         pca.fit(self.data)
-        pca_data = pca.transform(self.data)
-        print(pca_data)
+        pca_data = pd.DataFrame(pca.transform(self.data))
+        print(pca_data.head())
         pca_km = km.fit_predict(pca_data)
-
-        plt.scatter(
-        pca_data[y_km == 0, 0], pca_data[y_km == 0, 1],
-            s=50, c='lightgreen',
-            marker='s', edgecolor='black',
-            label='cluster 1'
-        )
-
-        plt.scatter(
-            pca_data[y_km == 1, 0], pca_data[y_km == 1, 1],
-            s=50, c='orange',
-            marker='o', edgecolor='black',
-            label='cluster 2'
-        )
-
-        plt.scatter(
-            pca_data[y_km == 2, 0], pca_data[y_km == 2, 1],
-            s=50, c='lightblue',
-            marker='v', edgecolor='black',
-            label='cluster 3'
-        )
-
-        # plot the centroids
-        plt.scatter(
-            km.cluster_centers_[:, 0], km.cluster_centers_[:, 1],
-            s=250, marker='*',
-            c='red', edgecolor='black',
-            label='centroids'
-        )
-        plt.legend(scatterpoints=1)
-        plt.grid()
-        plt.show()
-
 
 def main():
     KMEAN().predict()
